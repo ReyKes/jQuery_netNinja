46acff0f9adb4e4639d7d4af6711879107033cfb
Reykes <perceptionenhancement@gmail.com>
2019-06-02 16:33:04 +0530

    Lesson11_RemovingContent

---
+++ 11_Removing Content/PSD-to-HTML-1/Scripts/script.js
@@ -0,0 +1,13 @@
+
+/*
+$(".button").empty();
+$("#points-of-sale").empty();
+*/
+$(".button").remove();
+$("#contact img").remove();
+
+
+/*
+.empty()- empties the inner HTML of an element
+.remove()-removes the element completely
+*/
