c87351507c43c1de12fa06a364914f5c29193e63
Reykes <perceptionenhancement@gmail.com>
2019-06-02 10:07:26 +0530

    Lessons 1-7

---
+++ 1-7/PSD-to-HTML-1/Scripts/script.js
@@ -0,0 +1,48 @@
+/*$(document).ready(function()
+{
+  alert("jquery successfully loaded");  
+}
+);*/
+
+/*$("h3").css({border:"3px solid blue"});
+$(".wrapper").css({border: "2px solid green"});
+$("#contact").css({border:"10px solid blue"});*/
+
+/*$("header nav li:first").css({border:"2px solid white"});
+
+$("header nav li:last").css({border:"2px solid white"});
+
+$("header nav ul:first-child").css({border:"2px solid red"});
+
+$("header nav ul:last-child").css({border:"2px solid red"});*/
+
+/*$("header nav li:even").css({border:"2px solid yellow"});
+
+$("header nav li:odd").css({border:"2px solid green"});
+
+$("section:not('#contact')").css({border:"2px solid green"});
+
+$("#social-nav li:lt(3)").css({border:"2px solid blue"});
+
+$("#social-nav li:gt(2)").css({border:"2px solid red"});
+
+$("div[class]").css({border:"2px solid pink"});
+
+$("img[alt]").css({border:"2px solid red"});
+
+$("img[alt=quote]").css({border:"2px solid purple"});*/
+
+/*
+$("#social-nav").prev().css({border:"3px solid red"});
+$("#contact-methods").next().css({border:"3px solid blue"});
+
+$(".banner-title").parent().css({border:"3px solid red"});
+$(".banner-title").parents().css({border:"3px solid red"});
+*/
+
+/*
+$("#social-nav").children().css({border:"3px solid green"});
+
+$("#contact").find(".facebook").css({border:"3px solid purple"});
+*/
+/*$("#social-nav").closest(".wrapper").css({border:"3px solid orange"});*/
