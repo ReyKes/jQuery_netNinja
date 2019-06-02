ddb42c28bcff36eac8ed49cfbb864667bddcf07b
Reykes <perceptionenhancement@gmail.com>
2019-06-02 11:19:31 +0530

    Adding content using jQuery'

--- 8/PSD-to-HTML-1/Scripts/script.js
+++ 8/PSD-to-HTML-1/Scripts/script.js
@@ -1,3 +1,24 @@
-$("#contact-methods").css({border:"2px solid red"})
+/*$("#contact-methods").css({border:"2px solid red"})
     .next().css({border:"2px solid green"})
-    .closest("section").css({border:"2px solid blue"});
+    .closest("section").css({border:"2px solid blue"});*/
+
+var tweet="<div style='margin:20px 0;padding: 10px;background: #eee'>The big fiight live: Ham vs Cheese</div>";
+
+//$("#tweets div").append(tweet);
+//$("#tweets div").prepend(tweet);
+//$("#tweets div p").before(tweet);
+//$("#tweets div p").after(tweet);
+
+//$("#tweets div p").text(tweet);
+//$("#tweets div").html(tweet);
+
+/*Adding & Changing Content
+
+.append()  adds content to botton of the element
+.prepend() adds content to top of element
+.before() adds content to before element
+.after() adds content to after element
+.html() changes the whole html of the elemnt
+.text() changes the text of an element
+
+*/
